297437c3eecfe9fed2ed78d73d195de642d1f45f
Charlie Chapman <charliemchapman@gmail.com>
2018-06-09 20:18:12 -0500

    showing first episode that was accidentally being left off

--- src/components/PostSummaryList.js
+++ src/components/PostSummaryList.js
@@ -4,7 +4,7 @@ import styles from "../styles/post-summary.module.css";
 
 export default ({data}) => {
 
-    const episodes = data.allRssFeedItem.edges.slice(1, data.allRssFeedItem.edges.length-1);
+    const episodes = data.allRssFeedItem.edges.slice(1, data.allRssFeedItem.edges.length);
     const episodesJsx = episodes.map(episode=>{
         return <PostSummary episode={episode.node} />
     });
