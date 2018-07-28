5c5d12512090b323b328ef72309f389931949357
Charlie Chapman <charliemchapman@gmail.com>
2018-07-27 22:16:42 -0500

    updated wall of thanks

--- src/pages/patreon.js
+++ src/pages/patreon.js
@@ -11,10 +11,25 @@ export default () => (
         </a>
         <p>With your support we will be able to bring on better guests, improve quality, expand, and do more for our fans. Any support whether financially or by word of mouth is appreciated!</p>
         <h1>Wall of Thanks</h1>
-        <ul>
-            <li>Nicholas Lewis</li>
-            <li>Chad S</li>
-            <li>Dani W</li>
-        </ul>
+        <br></br>
+        <table class="thanks">
+            <tr><th>Patron</th><th>Patron Corner Episode</th></tr>
+            <tr>
+                <td>Nicholas Lewis</td>
+                <td><a href="http://atownfm.com/episodes/2018/03/27/episode-039.html">#39 - Nick's Corner</a></td>
+            </tr>
+            <tr>
+                <td>Chad S</td>
+                <td><a href="http://atownfm.com/episodes/2018/04/17/episode-041.html">#41 - Tolkien Corner, British Royalty, Chad's Corner</a></td>
+            </tr>
+            <tr>
+                <td>Dani W</td>
+                <td><em>Coming Soon...</em></td>
+            </tr>
+            <tr>
+                <td>Ben Maga</td>
+                <td><em>Coming Soon...</em></td>
+            </tr>
+        </table>
     </div>
 )
