865e9a3b7deee9cd5b7597cf1f9df3497fe9d1c5
Charlie Chapman <charliemchapman@gmail.com>
2019-02-21 07:21:13 -0600

    fixed bug when featured post was an episode

--- src/components/FeaturedEpisode.js
+++ src/components/FeaturedEpisode.js
@@ -3,21 +3,22 @@ import Link from "gatsby-link";
 import styles from "../styles/featured-post.module.scss"
 
 export default ({episode}) => { 
+  var post = episode.post;
   return (
   <div className={styles.container}>
     <div className={styles.content}>
         <h1>
-          <Link to={episode.fields.slug} className={styles.title}>
-            {episode.title}
+          <Link to={post.fields.slug} className={styles.title}>
+            {post.title}
           </Link>
         </h1>
-        <div className={styles.date}>{new Date(episode.pubDate).toLocaleDateString()}</div>
+        <div className={styles.date}>{new Date(post.pubDate).toLocaleDateString()}</div>
         <div className={styles.player}>
-            <img className={styles.image} src={episode.itunes.image}/>
-            <audio className={styles.audio} src={episode.enclosure.url} preload="none" controls/>
+            <img className={styles.image} src={post.itunes.image}/>
+            <audio className={styles.audio} src={post.enclosure.url} preload="none" controls/>
         </div>
-        <p>{episode.itunes.subtitle}</p>
-        <Link to={episode.fields.slug} className={styles.shownotes}>Show Notes</Link>
+        <p>{post.itunes.subtitle}</p>
+        <Link to={post.fields.slug} className={styles.shownotes}>Show Notes</Link>
     </div>  
   </div>
 )};
